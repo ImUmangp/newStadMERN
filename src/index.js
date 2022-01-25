722eb87b05fe84284e07cbe959b06d7ffd64a92d
Umang <umangpandey14@gmail.com>
2022-01-26 00:29:41 +0530

    getting started

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <>
+    <h1>Umang's Netflix pick</h1>
+    <p>Here's the list of my fav 6 netflix series</p>
+    <ol>
+      <li>Dark</li>
+      <li>Mr Robot</li>
+      <li>Lucifer</li>
+      <li>My Holo Love</li>
+      <li>Money Heist</li>
+    </ol>
+  </>,
+  document.getElementById("root")
+);
