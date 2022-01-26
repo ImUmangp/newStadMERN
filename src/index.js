d75316ff9ba2b9ea2ea8c8968b9df37115278738
Umang <umangpandey14@gmail.com>
2022-01-26 14:35:26 +0530

    challenge3

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css'
 
 const name ="UmangPandey";
 const currDate = new Date().toLocaleDateString();
 const currTime = new Date().toLocaleTimeString();
+
+let curDate=new Date();
+curDate=curDate.getHours();
+let greeting='';
+
+const cssStyle = { };
+
+if(curDate>=1 && curDate<12){
+  greeting='Good MOrning';
+  cssStyle.color ='green';
+}else if(curDate>=12 && curDate<19){
+   greeting='Good Afternoon';   
+  cssStyle.color ='Orange';
+}else{
+  greeting='Good Night';  
+  cssStyle.color ='Yellow';
+}
+
+
 
 ReactDOM.render(
   <>
@@ -22,7 +42,11 @@ ReactDOM.render(
     <h2>My name is {name}</h2>
     <p>Current Date is = {currDate}</p>
     <p>Current Time is = {currTime} </p>
-
+  <div>
+  <h1>Challenge#03</h1>
+  <p>Greets According to Time:</p>
+  <h3 >Hello Sir,<span style={cssStyle}>{greeting}</span></h3>  
+  </div>
   </>,
   document.getElementById("root")
 );
