89155b442ae492d21c1352b2aa8ebf62af6aced5
Umang <umangpandey14@gmail.com>
2022-01-26 12:55:31 +0530

    Challenge@

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const name ="UmangPandey";
+const currDate = new Date().toLocaleDateString();
+const currTime = new Date().toLocaleTimeString();
+
 ReactDOM.render(
   <>
-    <h1>Umang's Netflix pick</h1>
+  <h1>Challenge#1</h1>
+    <h2>Umang's Netflix pick</h2>
     <p>Here's the list of my fav 6 netflix series</p>
     <ol>
       <li>Dark</li>
@@ -12,6 +17,12 @@ ReactDOM.render(
       <li>My Holo Love</li>
       <li>Money Heist</li>
     </ol>
+ 
+  <h1>Challenge#2</h1>
+    <h2>My name is {name}</h2>
+    <p>Current Date is = {currDate}</p>
+    <p>Current Time is = {currTime} </p>
+
   </>,
   document.getElementById("root")
 );
